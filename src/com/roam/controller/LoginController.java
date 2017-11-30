b11deb152ffe5ea4a5b328fd42de2724c8890acb
lyy <lyylyymail@163.com>
2017-11-30 15:25:43 +0800

    commit just test

--- src/com/roam/controller/LoginController.java
+++ src/com/roam/controller/LoginController.java
@@ -23,54 +23,48 @@ public class LoginController {
 	@Autowired
 	ILoginService loginService;
 
-	/**
-	 *  register
-	 *  if the request URL contains register use this method
-	 *  
-	 *  ^(*￣(oo)￣)^: the request method must be post
-	 *  
-	 *  P.S:  note "@ModelAttribute("user")" because can't find why it exist
-	 */
 	@ResponseBody
 	@RequestMapping(value = "/register", method = RequestMethod.POST)
-	public User register(/*@ModelAttribute("user")*/ User user) throws Exception {
-		user.setPassword(EncrypUtil.encryptMD5(user.getPassword()));//encrypt the password by MD5
-		User resultUser = loginService.register(user);//register 
-		if (resultUser != null) {//if exist user : means register success, then return the register user
+	public User register(User user) throws Exception {
+		user.setPassword(EncrypUtil.encryptMD5(user.getPassword()));
+		User resultUser = loginService.register(user); 
+		if (resultUser != null) {
 			return resultUser;
 		} 
-		return null;//if insert database error, return null
+		return null;
 	}
 
-	/**
-	 *  login 
-	 *  if the request URL contains login use this method
-	 *  ^(*￣(oo)￣)^: the request method must be post
-	 */
 	@RequestMapping(value = "/login", method = RequestMethod.POST)
-	public ModelAndView login(User user, ModelAndView welcomeView) {
+	public ModelAndView login(User user, String remeberMe, ModelAndView welcomeView) {
 		UsernamePasswordToken token = new UsernamePasswordToken(user.getName(), user.getPassword());//user authentication
-		token.setRememberMe(true);//set rememberMe
-		Subject subject = SecurityUtils.getSubject();//current user
+		token.setRememberMe(remeberMe==null ? false : true );
+		Subject subject = SecurityUtils.getSubject();
 		try {
-			subject.login(token);//judge the user whether authenticated
-		} catch (ExcessiveAttemptsException e) {//exception : login count too more
-			welcomeView.setViewName("redirect:content/html/register.html");
+			subject.login(token);
+		} catch (ExcessiveAttemptsException e) {
+			welcomeView.setViewName("login");
+			welcomeView.addObject("isTipsShow","Y");
+			welcomeView.addObject("pwdErrorLoginTips","该用户登陆错误次数超过3次，请联系客服~");
 			return welcomeView;
-		} catch (AuthenticationException e) {
-			System.out.println("error:   " + e.getMessage());
+		} 
+		//认证失败
+		catch (AuthenticationException e) {
+			welcomeView.setViewName("login");
+			welcomeView.addObject("isTipsShow","Y");
+			welcomeView.addObject("pwdErrorLoginTips","该用户还有"+ e.getMessage() + "次登录机会~");
+			return welcomeView;
 		}
-		if (subject.isAuthenticated()) {//if the user is authenticated
+		if (subject.isAuthenticated()) {
 			Subject currentUser = SecurityUtils.getSubject();
-			if (currentUser.isPermitted("adminpermission")) {//judge the user permitted whether administrator
+			if (currentUser.isPermitted("adminpermission")) {
 				welcomeView.addObject("permission", "给管理员递茶");
-			} else if (currentUser.isPermitted("userpermission")) {//judge the user permitted whether user
+			} else if (currentUser.isPermitted("userpermission")) {
 				welcomeView.addObject("permission", "用户棒棒哒");
 			} else {//other
 				welcomeView.addObject("permission", "游客你好");
 			}
-			welcomeView.setViewName("resource/welcome");//set jump view name
-			welcomeView.addObject("welcomeUser", user);//set available object
+			welcomeView.setViewName("resource/welcome");
+			welcomeView.addObject("welcomeUser", user);
 		} 
 		return welcomeView;
 	}
@@ -79,45 +73,31 @@ public class LoginController {
 	// @RequiresRoles("admin")
 	// 权限注解
 	// @RequiresPermissions("adminpermission")
-	/**
-	 * lookForRes: check the user role
-	 * 
-	 * ^(*￣(oo)￣)^: the request method need be get
-	 */
 	@RequestMapping(value = "/lookForRes", method = RequestMethod.GET)
 	public ModelAndView lookForRes(ModelAndView view) {
 		Subject subject = SecurityUtils.getSubject();
-		if (subject.hasRole("admin")) {//if the user role is admin
+		if (subject.hasRole("admin")) {
 			view.setViewName("resource/res");
 			return view;
 		}
-		if (subject.hasRole("user")) {//if the user role is user
+		if (subject.hasRole("user")) {
 			view.setViewName("resource/userres");
 			return view;
 		}
-		view.setViewName("resource/nopermission");//other
+		view.setViewName("resource/nopermission");
 		return view;
 	}
-	
-	/**
-	 * logout 
-	 * 
-	 * ^(*￣(oo)￣)^: the request method need be get
-	 * 
-	 * @param view
-	 * @return
-	 */
+
 	@RequestMapping(value = "/logout", method = RequestMethod.GET)
 	public ModelAndView logout(ModelAndView view) {
 		Subject subject = SecurityUtils.getSubject();
 		try {
 			subject.logout();
 		} catch (AuthenticationException e) {
-			//why not use e.getMessage();??
-			e.printStackTrace();//在命令行打印异常信息在程序中出错的位置及原因
-
+			e.printStackTrace();
 		}
-		view.setViewName("login");
+		view.setViewName("redirect:content/html/login.html");
 		return view;
 	}
+	
 }
