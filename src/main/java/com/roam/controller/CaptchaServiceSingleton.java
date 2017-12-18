603c2534121f424ab1e2e726dedcaf5486a0c2eb
roam <roamup@163.com>
2017-12-18 19:13:06 +0800

    解决不能启动问题

---
+++ src/main/java/com/roam/controller/CaptchaServiceSingleton.java
@@ -0,0 +1,15 @@
+package com.roam.controller;
+
+import com.octo.captcha.service.captchastore.FastHashMapCaptchaStore;
+import com.octo.captcha.service.image.DefaultManageableImageCaptchaService;
+import com.octo.captcha.service.image.ImageCaptchaService;
+
+public class CaptchaServiceSingleton {
+	private static ImageCaptchaService instance = new DefaultManageableImageCaptchaService(
+			new FastHashMapCaptchaStore(), new GMailEngine(), 180, 100000, 75000);
+
+	public static ImageCaptchaService getInstance() {
+		return instance;
+	}
+
+}
